39e4bab88902732292da963cca1feadd37cbdcb5
4tlc <tcroniniv@gmail.com>
2022-10-15 22:14:36 -0400

    fix print red function

--- Printing.py
+++ Printing.py
@@ -1,5 +1,5 @@
 # grabbed from https://www.geeksforgeeks.org/print-colors-python-terminal/
-def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prRed(skk): print("\033[1m {skk} \033[00m" .format(skk))
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
 def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
