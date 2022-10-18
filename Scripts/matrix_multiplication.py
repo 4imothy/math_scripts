3e555e626cd8c205434417dfe3703afd5bca1764
4tlc <tcroniniv@gmail.com>
2022-10-18 12:01:57 -0400

    create function to gather data

--- Scripts/matrix_multiplication.py
+++ Scripts/matrix_multiplication.py
@@ -1,44 +1,35 @@
 from Printing import *
 import numpy as np
 
-# get matrix A
-prLightPurple("Matrix A")
-RA = int(input("Enter the number of rows: "))
-CA = int(input("Enter the number of columns: "))
-prCyan("Enter the entries in a single line (separated by space): ")
-entriesA = list(map(int, input().split()))
-try:
-    A = np.array(entriesA).reshape(RA, CA)
-except:
-    prRed("A is not properly formatted")
-    exit()
-prLightPurple("A: ")
-prLightGray(A)
+def gatherData(name):
+    prLightPurple("Matrix " + name)
+    numRow = int(input("Enter the number of rows: "))
+    numCol = int(input("Enter the number of columns: "))
+    prCyan("Enter the entries in a single line (separated by space): ")
+    try:
+        entries = list(map(int, input().split()))
+        matrix = np.array(entries).reshape(numRow, numCol)
+    except:
+        prRed(name + " is not properly formatted")
+        exit()
+    prLightPurple(name + ": ")
+    prLightGray(matrix)
+    print("----------")
+    return matrix
 
-print("----------")
-# set matrix B
-prLightPurple("Matrix B")
-RB = int(input("Enter the number of rows: "))
-if CA != RB:
-    prRed("# Columns of A ≠ # Rows Of B")
-    exit()
-CB = int(input("Enter the number of columns: "))
-prCyan("Enter the entries in a single line (separated by space): ")
-entriesB = list(map(int, input().split()))
-try:
-    B = np.array(entriesB).reshape(RB, CB)
-except:
-    prRed("B is not properly formatted")
-    exit()
-prLightPurple("B: ")
-prLightGray(B)
+A = gatherData("A")
+B = gatherData("B")
+if A.shape[1] != B.shape[0]:
+        prRed("# Columns of A ≠ # Rows of B")
+        exit()
 
 C = np.zeros((A.shape[0],B.shape[1]),dtype = int)
+
 for row in range(len(A)):
     for col in range(len(B[0])):
         for elt in range(len(B)):
             C[row, col] += A[row, elt] * B[elt, col]
 
-print("----------")
+
 prPurple("Answer:")
 prLightGray(C)
