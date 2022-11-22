93c6cdabc5e2cc4a23a0d33d9bf3df0cf1cc8768
Timothy Cronin <tcroniniv@gmail.com>
2022-11-22 11:56:45 -0500

    fix errors

--- Scripts/Inverse.py
+++ Scripts/Inverse.py
@@ -4,26 +4,48 @@ import numpy as np
 def main():
     M = gatherData("Matrix")
     # create identity matrix to operate on
-    Inv = invertMatrix(M, np.identity(M.shape[1]))
+    prPurple("Inverse Matrix=")
     try:
-        prPurple("Inverse Matrix=")
+        Inv = inverse(M)
     except:
         prRed("Not Invertible")
         exit()
-    prLightGray(Inv)
+    for i in range(0,len(Inv)):
+        prLightGray(Inv[i])
 
-def invertMatrix(AM, IM):
-    for fd in range(len(AM)):
-        fdScaler = 1.0 / AM[fd][fd]
-        for j in range(len(AM)):
-            AM[fd][j] *= fdScaler
-            IM[fd][j] *= fdScaler
-        for i in list(range(len(AM)))[0:fd] + list(range(len(AM)))[fd+1:]:
-            crScaler = AM[i][fd]
-            for j in range(len(AM)):
-                AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
-                IM[i][j] = IM[i][j] - crScaler * IM[fd][j]
-    return IM
+def eliminate(r1, r2, col, target=0):
+    fac = (r2[col]-target) / r1[col]
+    for i in range(len(r2)):
+        r2[i] -= fac * r1[i]
+
+def gauss(a):
+    for i in range(len(a)):
+        if a[i][i] == 0:
+            for j in range(i+1, len(a)):
+                if a[i][j] != 0:
+                    a[i], a[j] = a[j], a[i]
+                    break
+            else:
+                raise ValueError("Matrix is not invertible")
+        for j in range(i+1, len(a)):
+            eliminate(a[i], a[j], i)
+    for i in range(len(a)-1, -1, -1):
+        for j in range(i-1, -1, -1):
+            eliminate(a[i], a[j], i)
+    for i in range(len(a)):
+        eliminate(a[i], a[i], i, target=1)
+    return a
+
+def inverse(a):
+    tmp = [[] for _ in a]
+    for i,row in enumerate(a):
+        assert len(row) == len(a)
+        tmp[i].extend(row + [0]*i + [1] + [0]*(len(a)-i-1))
+    gauss(tmp)
+    ret = []
+    for i in range(len(tmp)):
+        ret.append(tmp[i][len(tmp[i])//2:])
+    return ret
 
 def gatherData(name):
     prPurple("Matrix " + name)
@@ -37,15 +59,22 @@ def gatherData(name):
         prRed("That is not a number")
         exit()
     try:
-        entries = list(map(np.double, input(makeCyan("Entries (seperated by a space): ")).split()))
-        matrix = np.array(entries).reshape(numRow, numCol)
+        entries = list(map(float , input(makeCyan("Entries (seperated by a space): ")).split()))
+        matrix = reshape_list(entries,numRow)
     except:
         prRed(name + " is not properly formatted")
         exit()
     prPurple(name + "=")
-    prLightGray(matrix)
+    for i in range(0,len(matrix)):
+        prLightGray(matrix[i])
     prYellow("------------")
     return matrix
+
+def reshape_list(L, xsize, ysize=None, fillna=False):
+    gap = []
+    v, r = divmod(len(L),xsize)
+    gap = [None]*r
+    return list(map(list, zip(*[iter(L+gap)]*xsize)))
 
 if __name__ == "__main__":
     main()
