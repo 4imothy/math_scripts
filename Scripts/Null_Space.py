fb7440c78af9a84119e9e15ab7f6cb6a72c22857
Timothy Cronin <tcroniniv@gmail.com>
2022-12-03 16:59:05 -0500

    remove np from gathering data

--- Scripts/Null_Space.py
+++ Scripts/Null_Space.py
@@ -1,6 +1,5 @@
 from scipy.linalg import null_space
 from Printing import *
-import numpy as np
 
 def main():
     m = gatherData("Matrix")
@@ -16,15 +15,26 @@ def gatherData(name):
         prRed("That is not a number")
         exit()
     try:
-        entries = list(map(np.double, input(makeCyan("Entries (seperated by a space): ")).split()))
-        matrix = np.array(entries).reshape(numRow, numCol)
+        entries = list(map(float , input(makeCyan("Entries (seperated by a space): ")).split()))
+        matrix = reshape_list(entries,numCol)
     except:
         prRed(name + " is not properly formatted")
         exit()
     prPurple(name + "=")
-    prLightGray(matrix)
+    print_matrix(matrix)
     prYellow("------------")
     return matrix
+
+def reshape_list(L, xsize):
+    gap = []
+    v, r = divmod(len(L),xsize)
+    gap = [None]*r
+    return list(map(list, zip(*[iter(L+gap)]*xsize)))
+
+def print_matrix(matrix):
+        for i in range(0,len(matrix)):
+            prLightGray(matrix[i])
+
 
 if __name__ == "__main__":
     main()
